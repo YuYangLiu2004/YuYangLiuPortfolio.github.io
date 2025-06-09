5e1955cb298f6bc3d1dea2c2b09e5f6f0abf2c63
YuYangLiu2004 <yliu863@uottawa.ca>
2025-06-08 21:15:23 -0400

    back to port

--- skyscanner-lite-prototype/src/components/Header.js
+++ skyscanner-lite-prototype/src/components/Header.js
@@ -5,7 +5,14 @@ function Header() {
         <header>
             <nav className="navbar navbar-expand-lg bg-light shadow-sm">
                 <div className="container">
-                    <a className="navbar-brand fs-4" href="/"><i className="bi bi-airplane-engines-fill"></i> G36 Air</a>
+                    <a className="navbar-brand fs-4" href="HomeScreen.js"><i className="bi bi-airplane-engines-fill"></i> G36 Air</a>
+
+                    <div className="ms-auto">
+                        <a href="https://yuyangliu2004.github.io/YuYangLiuPortfolio.github.io/" className="btn btn-outline-primary">
+                            <i className="bi bi-person-circle me-2"></i>
+                            My Portfolio
+                        </a>
+                    </div>
                 </div>
             </nav>
         </header>
